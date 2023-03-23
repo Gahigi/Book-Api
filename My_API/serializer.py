6a4720af08d91b8940fb2babe708cd9eeef30eea
Gahigi <jeni02893@gmail.com>
2023-03-23 11:39:39 +0200

    Changes in View

--- My_API/serializer.py
+++ My_API/serializer.py
@@ -4,4 +4,4 @@ from .models import *
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['title', 'subtitle', 'author', 'isbn']
+        fields = ['id','title', 'subtitle', 'author', 'isbn']
